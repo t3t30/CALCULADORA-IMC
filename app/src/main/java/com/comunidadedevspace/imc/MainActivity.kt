3080d96784429bfa4eb9e1dd273c24af4d244a66
t3t30 <mateusbie6@gmail.com>
2024-11-21 17:29:19 -0300

    Tela inicial edittext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,27 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+         * recuperar componentes edittext altura e peso
+         * peso/(altura*altura)=resultado
+         * pegar resultado, comparar tabela
+         * mostrar classificação
+         */
+        val edtpeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtaltura = findViewById<TextInputEditText>(R.id.edt_altura)
+        val btncalcular = findViewById<Button>(R.id.btn_calcular)
+        btncalcular.setOnClickListener {
+            val peso = edtpeso.text
+            val altura = edtaltura.text
+
+        }
     }
 }
